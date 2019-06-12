4a026f529dbc5b8f7e5c25d3f15ea029e3c1fd26
Taylor Cooke <taylorcooke@gmail.com>
2019-06-11 21:21:04 -0400

    trying to add second search function

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function queryAll (selector) {
 }
 
 const search = query('.search_button')
-const cands = query('.cands')
+const cands = query('.cand_id')
+const candidate = query('.candidate')
 // const candApi = 'http://www.opensecrets.org/api/?method=candSummary&cid=N00007360&cycle=2018&apikey=fb22899678d9793a7656d81e78055803'
 
 function getCandID (n) {
@@ -17,8 +18,24 @@ function getCandID (n) {
     method: 'GET',
     headers: {
       'X-API-Key': '5kMXtbKMfmdYhn87JENQ9vcAzqXDonYCYzmYZazd'
-    },
-    
+    }
+
+  }).then(function (response) {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  return promise
+}
+
+function getCand (n) {
+  const promise = fetch(`https://api.propublica.org/campaign-finance/v1/2016/candidates/${n}`, {
+    method: 'GET',
+    headers: {
+      'X-API-Key': '5kMXtbKMfmdYhn87JENQ9vcAzqXDonYCYzmYZazd'
+    }
+
   }).then(function (response) {
     if (!response.ok) {
       throw Error(response.statusText)
@@ -32,21 +49,39 @@ function updateID (n) {
   getCandID(n).then(function (reps) {
     cands.innerHTML = ''
     for (let rep of reps.results) {
-      addRepInfo(rep)
+      addCandID(rep)
     }
   })
 }
 
-function addRepInfo (name) {
+function candInfo (n) {
+  getCand(n).then(function (rep) {
+    candidate.innerHTML = ''
+    addRepInfo(rep.results)
+  })
+}
+
+function addCandID (name) {
   let repName = document.createElement('div')
 
   cands.append(repName)
 
-  repName.innerHTML = `<div>CID: "${name.candidate.name}"</div>`
+  repName.innerHTML = `<div>Name: ${name.candidate.name}, CID: ${name.candidate.id}</div>`
+}
+
+function addRepInfo (name) {
+  let rep = document.createElement('div')
+
+  candidate.append(rep)
+  console.log(candidate);
+  rep.innerHTML = `<div>Name: ${name.first_name}</div>`
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  query('.search').addEventListener('change', function (e) {
+  query('.cand_id').addEventListener('change', function (e) {
     updateID(event.target.value)
+  })
+  query('.candidate').addEventListener('change', function (e) {
+    candInfo(event.target.value)
   })
 })
