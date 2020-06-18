429eb643f470779b6b116bfa7886baeca1be77b6
Taylor Cooke <tcooke@relias.com>
2020-06-18 11:08:24 -0400

    partisan box centered

--- main.js
+++ main.js
@@ -260,11 +260,11 @@ function addRepDetails(rep) {
   repDetails.append(repInfo);
   repInfo.classList.add("repInfo");
 
-  repInfo.innerHTML = `<div>${rep.first_name} ${rep.last_name}</div><br>
+  repInfo.innerHTML = `<div>${rep.first_name} ${rep.last_name}</div><br><div class="card_container">
   <div class="front">Partisan info</div>
   <div class="back"><div>Cook report District rating - ${rep.roles[0].cook_pvi}</div><br>
   <div>DW_Nominate rating - ${rep.roles[0].dw_nominate}</div><br>
-  <div>Votes with party: ${rep.roles[0].votes_with_party_pct}</div></div>`;
+  <div>Votes with party: ${rep.roles[0].votes_with_party_pct}</div></div></div>`;
 }
 
 function showRepDetails(n) {
