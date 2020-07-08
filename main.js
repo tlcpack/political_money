d7a8fc287fb8f2b195d3b41dd31391a2cbc5b5e9
Taylor Cooke <tcooke@relias.com>
2020-07-08 13:49:28 -0400

    default styling

--- main.js
+++ main.js
@@ -330,16 +330,26 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   const clock = document.getElementById(id);
-  const timeinterval = setInterval(() => {
+  const daysSpan = clock.querySelector('.days');
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
     const t = getTimeRemaining(endtime);
-    clock.innerHTML = 'days: ' + t.days + '<br>' +
-                      'hours: '+ t.hours + '<br>' +
-                      'minutes: ' + t.minutes + '<br>' +
-                      'seconds: ' + t.seconds;
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
     if (t.total <= 0) {
       clearInterval(timeinterval);
     }
-  }, 1000)
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
 }
 initializeClock('clockdiv', deadline);
 
