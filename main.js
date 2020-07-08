77030687cac583cbfe499010c6a8aba9f3facd6a
Taylor Cooke <tcooke@relias.com>
2020-07-08 13:25:19 -0400

    timer counting down! to work on styling

--- main.js
+++ main.js
@@ -313,6 +313,35 @@ function getDW(id) {
   return dw;
 }
 
+// countdown functionality
+const deadline = '2020-11-3';
+
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total/1000) % 60);
+  const minutes = Math.floor((total/1000/60) % 60);
+  const hours = Math.floor((total/(1000*60*60)) % 24);
+  const days = Math.floor(total/(1000*60*60*24));
+
+  return {
+    total, days, hours, minutes, seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const timeinterval = setInterval(() => {
+    const t = getTimeRemaining(endtime);
+    clock.innerHTML = 'days: ' + t.days + '<br>' +
+                      'hours: '+ t.hours + '<br>' +
+                      'minutes: ' + t.minutes + '<br>' +
+                      'seconds: ' + t.seconds;
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }, 1000)
+}
+initializeClock('clockdiv', deadline);
 
 document.addEventListener("DOMContentLoaded", function () {
   // query(".cand_id").addEventListener("keyup", function (e) {
@@ -333,7 +362,7 @@ document.addEventListener("DOMContentLoaded", function () {
   query(".state").addEventListener("change", function (e) {
     findHouseRep(event.target.value);
   });
-  query(".repDetail").addEventListener("change", function (e) {
-    showRepDetails(event.target.value);
-  });
+  // query(".repDetail").addEventListener("change", function (e) {
+  //   showRepDetails(event.target.value);
+  // });
 });
