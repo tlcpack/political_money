d0306dd2c1ff6ab4d24ba5504fc9e93793ede6e5
Taylor Cooke <tcooke@relias.com>
2020-10-14 14:59:45 -0400

    refactoring, removing unused code

--- main.js
+++ main.js
@@ -9,105 +9,11 @@ function query(selector) {
 const cands = query(".cand_search");
 const candidate = query(".candidates");
 const stateRep = query(".stateRep");
-const donors = query(".donors");
-const committees = query(".committees");
-const zips = query(".zips");
-const social = query(".social");
 const houseReps = query(".houseReps");
 const repDetails = query(".repDetails");
 
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-function getCandID(n) {
-  const promise = fetch(
-    `https://api.propublica.org/campaign-finance/v1/2016/candidates/search.json?query=${n}`,
-    {
-      method: "GET",
-      headers: {
-        "X-API-Key": "5kMXtbKMfmdYhn87JENQ9vcAzqXDonYCYzmYZazd",
-      },
-    }
-  ).then(function (response) {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  });
-  return promise;
-}
-
-function getCongressID(n) {
-  const promise = fetch(
-    `https://api.propublica.org/congress/v1/2016/candidates/search.json?query=${n}`,
-    {
-      method: "GET",
-      headers: {
-        "X-API-Key": "LJgQYwDfuk9k4KkmrZj1DcbU6AJ5nawhLGaiN5oM",
-      },
-    }
-  ).then(function (response) {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  });
-  return promise;
-}
-
-function getCandFinanceInfo(n) {
-  const promise = fetch(
-    `https://api.propublica.org/campaign-finance/v1/2016/candidates/${n}.json`,
-    {
-      method: "GET",
-      headers: {
-        "X-API-Key": "5kMXtbKMfmdYhn87JENQ9vcAzqXDonYCYzmYZazd",
-      },
-    }
-  ).then(function (response) {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  });
-  return promise;
-}
-
-function candFromState(n) {
-  const promise = fetch(
-    `https://api.propublica.org/campaign-finance/v1/2016/races/${n}.json`,
-    {
-      method: "GET",
-      headers: {
-        "X-API-Key": "5kMXtbKMfmdYhn87JENQ9vcAzqXDonYCYzmYZazd",
-      },
-    }
-  ).then(function (response) {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  });
-  return promise;
-}
-
-function getDonors(n) {
-  const promise = fetch(
-    `https://api.propublica.org/campaign-finance/v1/2016/candidates/${n}/independent_expenditures.json`,
-    {
-      method: "GET",
-      headers: {
-        "X-API-Key": "5kMXtbKMfmdYhn87JENQ9vcAzqXDonYCYzmYZazd",
-      },
-    }
-  ).then(function (response) {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  });
-  return promise;
 }
 
 function getAllHouse() {
@@ -145,87 +51,6 @@ function getSpecificHouseMember(n) {
   });
   // console.log(promise);
   return promise;
-}
-
-function updateID(n) {
-  getCongressID(n).then(function (reps) {
-    cands.innerHTML = "";
-    for (let rep of reps.results) {
-      addCandID(rep);
-    }
-  });
-}
-
-function addCandID(name) {
-  let repName = document.createElement("div");
-
-  cands.append(repName);
-
-  repName.innerHTML = `<div>Name: ${name.candidate.name}, CID: ${name.candidate.id}</div>`;
-}
-
-function candInfo(n) {
-  getCandFinanceInfo(n).then(function (id) {
-    candidate.innerHTML = "";
-    addRepInfo(id.results[0]);
-  });
-}
-
-function addRepInfo(name) {
-  let repID = document.createElement("div");
-  if (name.district === null) {
-    repID.innerHTML = "<div>Info not found, please re-search</div>";
-  }
-  const repInfo = name.district.split("/");
-  const distState = repInfo[2];
-  const repDistrict = repInfo[4].substr(0, 2);
-
-  candidate.append(repID);
-
-  repID.innerHTML = `<div>Name: ${
-    name.name
-  }, ${distState}-${repDistrict}</div><div><br>Contributions from individuals: $${numberWithCommas(
-    name.total_from_individuals
-  )}<br>Contributions from PACs: $${numberWithCommas(
-    name.total_from_pacs
-  )}</div>`;
-}
-
-// function fromState(n) {
-//   candFromState(n).then(function (reps) {
-//     stateRep.innerHTML = "";
-//     for (let rep of reps.results) {
-//       idFromState(rep);
-//     }
-//   });
-// }
-
-function idFromState(name) {
-  let repState = document.createElement("div");
-
-  stateRep.append(repState);
-
-  repState.innerHTML = `<div>Name: ${name.candidate.name}, CID: ${name.candidate.id}</div>`;
-}
-
-function listDonors(n) {
-  getDonors(n).then(function (orgs) {
-    cands.innerHTML = "";
-    donors.innerHTML = "";
-    for (let org of orgs.results) {
-      if (org.support_or_oppose === "S") {
-        addDonors(org);
-      }
-    }
-  });
-}
-
-function addDonors(name) {
-  let candDonor = document.createElement("div");
-
-  donors.append(candDonor);
-
-  candDonor.innerHTML = `<div>Name: ${name.fec_committee_name}</div>`;
 }
 
 function addRepName(rep) {
@@ -357,25 +182,7 @@ function initializeClock(id, endtime) {
 initializeClock('clockdiv', deadline);
 
 document.addEventListener("DOMContentLoaded", function () {
-  // query(".cand_id").addEventListener("keyup", function (e) {
-  //   updateID(event.target.value);
-  // });
-  // query(".state").addEventListener("change", function (e) {
-  //   fromState(event.target.value);
-  // });
-  // query(".candidate").addEventListener("change", function (e) {
-  //   candInfo(event.target.value);
-  // });
-  // query(".candidate").addEventListener("change", function (e) {
-  //   listDonors(event.target.value);
-  // });
-  // query(".candidate").addEventListener("change", function (e) {
-  //   showCandidateDetails(event.target.value);
-  // });
   query(".state").addEventListener("change", function (e) {
-    findHouseRep(event.target.value);
+    findHouseRep(e.target.value);
   });
-  // query(".repDetail").addEventListener("change", function (e) {
-  //   showRepDetails(event.target.value);
-  // });
 });
